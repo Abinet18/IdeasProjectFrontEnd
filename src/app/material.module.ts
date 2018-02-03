8e471910708852b9129c0a280dd957c9223e5944
unknown <abinet.tafa@gmail.com>
2018-02-03 17:03:55 -0600

    navigation with angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,17 +3,17 @@ import { CommonModule } from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule,MatFormFieldModule,MatInputModule,MatRadioModule,MatSelectModule,MatSlideToggleModule,MatMenuModule} from '@angular/material';
-import {MatCardModule,MatChipsModule,MatTableModule} from '@angular/material';
+import {MatCardModule,MatChipsModule,MatTableModule,MatListModule} from '@angular/material';
 
 
 @NgModule(
     {
         imports:[MatButtonModule,
              MatCheckboxModule,MatFormFieldModule,MatInputModule,MatRadioModule,
-             MatSelectModule,MatSlideToggleModule,MatMenuModule,MatCardModule,MatChipsModule,MatTableModule],
+             MatSelectModule,MatSlideToggleModule,MatMenuModule,MatCardModule,MatChipsModule,MatTableModule,MatListModule],
         exports:[MatButtonModule, 
             MatCheckboxModule,MatFormFieldModule,MatInputModule,MatRadioModule
-            ,MatSelectModule,MatSlideToggleModule,MatMenuModule,MatCardModule,MatChipsModule,MatTableModule]
+            ,MatSelectModule,MatSlideToggleModule,MatMenuModule,MatCardModule,MatChipsModule,MatTableModule,MatListModule]
 
     })
     export class MaterialModule
