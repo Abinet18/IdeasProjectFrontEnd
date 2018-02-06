fe1cf8d33976cda24282d7169e4b004b79e8bb2b
brianokuku <35316876+brianokuku@users.noreply.github.com>
2018-02-06 17:03:06 -0600

    Interface updates

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -16,13 +16,10 @@ import { Router } from '@angular/router';
 
 export class HomepageComponent implements OnInit {
   blogPosts;
-<<<<<<< HEAD
   home;
   searchForm;
   
   
-=======
->>>>>>> 6f509812f0efdeb7d0d8e1608a54721969a73f25
 
   constructor(private fb:FormBuilder, private data:DbService,private router:Router) {
    this.home=true;
