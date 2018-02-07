52d1c951cdf6e9202723df4c012f8959e6ab10f4
Abinet18 <35442169+Abinet18@users.noreply.github.com>
2018-02-07 04:34:04 -0600

    Update login.component.ts

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -11,10 +11,11 @@ import { Router } from '@angular/router';
   styleUrls: ['login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+//@Abinet 
+//create a form group for login 
   loginForm:FormGroup;
-  message:string;
-  previousUrl:string;
+  message:string;  //to hold message of success or failure on login
+  previousUrl:string;  // to hold the url that redirected to login if there is one
   constructor(private fb:FormBuilder,public data:DbService,private router:Router) { 
     this.loginForm=fb.group({
       'username':['',Validators.required],
@@ -34,26 +35,27 @@ export class LoginComponent implements OnInit {
 
   onLoginSubmit()
   {
+    //construct the user object and send to to the backend for checking the credentials
     let user={username:this.loginForm.controls.username.value,password:this.loginForm.controls.password.value};
     this.data.login(user).subscribe(
       (res)=>{
-        if(res==null)
+        if(res==null) //No response from backend
         {
           console.log("Http Error occured");
         }
-        else if(res.success)
+        else if(res.success)  //Login successful
         {
         console.log(res);
-        this.data.storeUserData(res.token,res.user);
-        let url=this.previousUrl?this.previousUrl:'/';
-        this.data.message="Welcome " + res.user.username;
-        this.data.messageClass="alert alert-info";
-        console.log(url);
-        this.router.navigate([url]);
+        this.data.storeUserData(res.token,res.user);  //store user token
+        let url=this.previousUrl?this.previousUrl:'/'; //check if user was redirected
+        this.data.message="Welcome " + res.user.username; //set the message for the home page
+        this.data.messageClass="alert alert-info";        
+        console.log(url);                                  
+        this.router.navigate([url]);//navigate to either home page or the redirected url if it exists
         }      
        else
       {
-        this.message="Invalid username or password";
+        this.message="Invalid username or password"; //set message on response with failed login status
       }
     });
   }
